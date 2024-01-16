fd07b52e29c89c3f13b3f7e69bf77aacf8c46ae1
Elyageen <yagosalih452@gmail.com>
2024-01-17 01:34:39 +0300

    addv

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
+for i in range(ord("z"), ord("a") - 1, -1):
     diff = 0 if i % 2 == 0 else 32
-    print(f'{chr(i - diff)}', end='')
+    print("{}".format(chr(i - diff)), end="")
