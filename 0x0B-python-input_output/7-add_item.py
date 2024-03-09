74b3079c97ddc5081bcb8959763c9f071a4f1d21
Elyageen <yagosalih452@gmail.com>
2024-03-09 15:14:43 +0300

    task 006-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function that append item an Object from a “JSON file”"""
+"""Module for appending items to a JSON file."""
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
