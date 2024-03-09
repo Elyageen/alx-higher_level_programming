aca222445ea64c29f55ef07b7d502d0c75286895
Elyageen <yagosalih452@gmail.com>
2024-03-09 14:13:54 +0300

    task 002

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ function that write a text file (UTF8) and prints it to stdout """
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
