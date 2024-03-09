6a5c100a9e0ef8bf56dd1ef3f26f1ba699857162
Elyageen <yagosalih452@gmail.com>
2024-03-09 15:23:38 +0300

    task 008

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function that returns the dictionary description"""
 
+
 def class_to_json(obj):
     """ function that returns the dictionary description"""
     return obj.__dict__.copy() if hasattr(obj, "__dict__") else {}
