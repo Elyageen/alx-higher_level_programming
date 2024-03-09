4c46c337a132440d99fda499bafef6d431ab46b3
Elyageen <yagosalih452@gmail.com>
2024-03-09 15:05:20 +0300

    task 006-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-""" ADDEND JSON"""
+"""function that append item an Object from a “JSON file”"""
 
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    try:
-        json_list = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        json_list = []
+try:
+    json_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    json_list = []
 
-    json_list.extend(sys.argv[i] for i in range(1, len(sys.argv)))
-    save_to_json_file(json_list, "add_item.json")
+json_list.extend(sys.argv[i] for i in range(1, len(sys.argv)))
+save_to_json_file(json_list, "add_item.json")
