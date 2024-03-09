544034806bfa1f878c494cf26cb199458a1f22bf
Elyageen <yagosalih452@gmail.com>
2024-03-09 14:18:38 +0300

    task 003

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
 
 import json
-
-"""function that returns the JSON representation of an object (string)"""
 
 
 def to_json_string(my_obj):
