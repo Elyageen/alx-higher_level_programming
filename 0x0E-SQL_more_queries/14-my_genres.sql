fe795e31d42c31209cb693698ed30d01bca6efdb
Elyageen <yagosalih452@gmail.com>
2024-03-28 02:44:25 +0300

    task 1-18

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
 -- 14-my_genres.sql
 SELECT tv_genres.name
-FROM tv_show_genres
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
