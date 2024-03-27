133e20b3ec2a4d0869a430d707b4393c51bd9c9c
Elyageen <yagosalih452@gmail.com>
2024-03-28 02:18:31 +0300

    task 16

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 
 -- Convert database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE hbtn_0c_0
 -- Convert table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
