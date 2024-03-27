935b04c000319ba5160ec1600ce5a30cb3c80b59
Elyageen <yagosalih452@gmail.com>
2024-03-28 02:52:29 +0300

    task 1-18

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,11 @@
+-- 8-cities_of_california_subquery.sql
+
 SELECT `name`, SUM(`rate`) AS `rating`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id`
-INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id`
-GROUP BY `name`
-ORDER BY `rating` DESC;
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
