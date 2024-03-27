d04be554baab9e86320a65a09cdea04eb584c03a
Elyageen <yagosalih452@gmail.com>
2024-03-28 02:53:14 +0300

    task 1-18

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,6 @@
-SELECT tv_shows.title, SUM(rating) AS rating_sum FROM tv_shows INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id GROUP BY tv_shows.title ORDER BY rating_sum DESC;
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
